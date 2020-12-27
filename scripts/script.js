a430e6cb168cd5bd3e7eb244c162a0cd82dab707
Julia <53926454+Ulia-lab@users.noreply.github.com>
2020-12-27 22:31:05 +0700

    fix name style

--- scripts/script.js
+++ scripts/script.js
@@ -5,20 +5,20 @@ let closeButton = page.querySelector('.edit__button_type_close');
 let formElement = document.querySelector('.edit__input');
 let saveButton = page.querySelector('.edit__save-button');
 let nameProfile = page.querySelector('.profile__name');
-let description = page.querySelector('.profile__description');
+let descriptionProfile = page.querySelector('.profile__description');
 let inputName = page.querySelector('.edit__input_text_name');
 let inputDescription = page.querySelector('.edit__input_text_description');
 
 function openPopup() {
     inputName.value = nameProfile.textContent;
-    inputDescription.value = description.textContent;
+    inputDescription.value = descriptionProfile.textContent;
     editPopup.classList.add('edit_active');
 }
 
 function handleFormSubmit (evt) {
     evt.preventDefault();
     nameProfile.textContent = inputName.value;
-    description.textContent = inputDescription.value;
+    descriptionProfile.textContent = inputDescription.value;
     editPopup.classList.remove('edit_active'); 
 }
 
